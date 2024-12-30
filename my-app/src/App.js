27beebacd62e07f061d52e334bd0cbf652362872
SonjaCodes <sonja@kolmer.cc>
2024-12-30 14:29:19 +0100

    added css color to winning squares and status

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,12 @@
 import { useState } from "react";
+import "./index.css";
 
-function Square({ value, onSquareClick }) {
+function Square({ value, onSquareClick, winner }) {
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button
+      className="square"
+      style={{ backgroundColor: winner ? "green" : "white" }}
+      onClick={onSquareClick}>
       {value}
     </button>
   );
@@ -33,21 +37,59 @@ function Board({ xIsNext, squares, onPlay }) {
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        <Square
+          value={squares[0]}
+          onSquareClick={() => handleClick(0)}
+          winner={winner && winner === squares[0]}
+        />
+        <Square
+          value={squares[1]}
+          onSquareClick={() => handleClick(1)}
+          winner={winner && winner === squares[1]}
+        />
+        <Square
+          value={squares[2]}
+          onSquareClick={() => handleClick(2)}
+          winner={winner && winner === squares[2]}
+        />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        <Square
+          value={squares[3]}
+          onSquareClick={() => handleClick(3)}
+          winner={winner && winner === squares[3]}
+        />
+        <Square
+          value={squares[4]}
+          onSquareClick={() => handleClick(4)}
+          winner={winner && winner === squares[4]}
+        />
+        <Square
+          value={squares[5]}
+          onSquareClick={() => handleClick(5)}
+          winner={winner && winner === squares[5]}
+        />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        <Square
+          value={squares[6]}
+          onSquareClick={() => handleClick(6)}
+          winner={winner && winner === squares[6]}
+        />
+        <Square
+          value={squares[7]}
+          onSquareClick={() => handleClick(7)}
+          winner={winner && winner === squares[7]}
+        />
+        <Square
+          value={squares[8]}
+          onSquareClick={() => handleClick(8)}
+          winner={winner && winner === squares[8]}
+        />
       </div>
-      <div className="status">{status}</div>
+      <div className="status" style={{ color: winner ? "green" : "black" }}>
+        {status}
+      </div>
     </>
   );
 }
@@ -63,8 +105,6 @@ export default function Game() {
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
   }
-
-
 
   function resetGame() {
     setHistory([Array(9).fill(null)]);
